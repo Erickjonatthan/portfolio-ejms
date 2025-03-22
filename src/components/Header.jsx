8589accc2dbf7022e7a67ccf4f6d5a4027db0798
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-03-22 18:17:46 -0300

    mudando biblioteca de animacao e estilzando a barra de scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion as Motion, useScroll, useMotionValueEvent } from "motion/react";
+import { motion as Motion, useScroll, useMotionValueEvent, AnimatePresence } from "framer-motion";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,7 +26,7 @@ export default function Header() {
 
   return (
     <Motion.header
-      className="w-full text-white p-4 flex items-center justify-between bg-inherit"
+      className="w-full text-white p-4 flex items-center justify-between bg-inherit fixed top-0 z-50"
       animate={{ opacity: scrollDirection === 'down' ? 0 : 1, y: scrollDirection === 'down' ? -20 : 0 }}
       transition={{ duration: 0.5 }}
     >
@@ -55,21 +55,29 @@ export default function Header() {
           </svg>
         )}
       </button>
-      {isMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md flex flex-col items-center justify-center z-50 overflow-hidden">
-          <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-            <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
-          </button>
-          <ul className="space-y-4 text-2xl">
-            <li><button onClick={() => scrollToSection('about-me')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left">Sobre Mim</button></li>
-            <li><button onClick={() => scrollToSection('projects')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left">Projetos</button></li>
-            <li><button onClick={() => scrollToSection('skills')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left">Habilidades</button></li>
-            <li><button onClick={() => scrollToSection('contact')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left mr-4">Contato</button></li>
-          </ul>
-        </div>
-      )}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <Motion.div
+            className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md flex flex-col items-center justify-center z-50 overflow-hidden"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+          >
+            <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
+              <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            </button>
+            <ul className="space-y-4 text-2xl">
+              <li><button onClick={() => scrollToSection('about-me')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left">Sobre Mim</button></li>
+              <li><button onClick={() => scrollToSection('projects')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left">Projetos</button></li>
+              <li><button onClick={() => scrollToSection('skills')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left">Habilidades</button></li>
+              <li><button onClick={() => scrollToSection('contact')} className="relative text-2xl hover:text-neonBlue hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left mr-4">Contato</button></li>
+            </ul>
+          </Motion.div>
+        )}
+      </AnimatePresence>
     </Motion.header>
   );
 }
