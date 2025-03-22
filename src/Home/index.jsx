3b1cb0ded354e425cce4593269e38d134da2fe64
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-03-22 14:09:39 -0300

    adiciona carregamento de imagem de fundo enquanto o vídeo não está pronto

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -9,20 +9,38 @@ import Contact from "../components/Contact";
 import Technologies from "../components/Technologies";
 import Welcome from "../components/Welcome"; 
 import backgroundVideo from "../assets/fundo-video.mp4";
+import backgroundImage from "../assets/fundo-imagem.png"; // Substitua pelo nome do seu arquivo de imagem
 
 export default function Home() {
   useDocumentTitle("Home Page");
 
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    const videoElement = document.getElementById("background-video");
+    videoElement.onloadeddata = () => {
+      setIsVideoLoaded(true);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="relative w-full h-screen">
+        {!isVideoLoaded && (
+          <img
+            src={backgroundImage}
+            alt="Background"
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+        )}
         <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          id="background-video"
+          className={`absolute top-0 left-0 w-full h-full object-cover ${isVideoLoaded ? 'block' : 'hidden'}`}
           src={backgroundVideo}
           autoPlay
           loop
           muted
-          preload="auto" // Adiciona o atributo preload
+          preload="auto"
         />
         <div className="relative z-10 w-full h-full flex flex-col">
           <Header />
