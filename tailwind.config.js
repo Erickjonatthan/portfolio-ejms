fb28e7b442267bc7656e3c715af4d32a50da2b79
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-03-22 17:06:19 -0300

    adiciona componente BackgroundVideo e configura animações e cores no Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        underline: {
+          '0%': { transform: 'scaleX(0)', transformOrigin: 'left' },
+          '100%': { transform: 'scaleX(1)', transformOrigin: 'left' },
+        },
+      },
+      animation: {
+        underline: 'underline 0.25s ease-out forwards',
+      },
+      colors: {
+        neonBlue: '#00f0ff',
+      },
+      boxShadow: {
+        neon: '0 0 5px #00f0ff, 0 0 10px #00f0ff, 0 0 20px #00f0ff, 0 0 40px #00f0ff',
+      },
+    },
   },
   plugins: [],
 }
