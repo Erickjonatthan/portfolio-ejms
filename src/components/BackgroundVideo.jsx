96fab911e571997a286c351fbf14b4bf79e35ae4
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-03-26 18:01:28 -0300

    adiciona lógica para exibir imagem antes do vídeo de fundo e ajusta condição de visibilidade do vídeo

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -7,17 +7,25 @@ import backgroundImage from "../assets/fundo-imagem.png";
 export default function BackgroundVideo() {
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [showImage, setShowImage] = useState(true);
 
   useEffect(() => {
     const videoElement = document.getElementById("background-video");
     videoElement.onloadeddata = () => {
       setIsVideoLoaded(true);
     };
+
+    // Mostrar a imagem por alguns segundos antes de exibir o vídeo
+    const timer = setTimeout(() => {
+      setShowImage(false);
+    }, 3000); // 3000ms = 3 segundos
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="relative w-full h-screen">
-      {(!isVideoLoaded || isMenuOpen) && (
+      {(showImage || !isVideoLoaded || isMenuOpen) && (
         <img
           src={backgroundImage}
           alt="Background"
@@ -26,7 +34,7 @@ export default function BackgroundVideo() {
       )}
       <video
         id="background-video"
-        className={`absolute top-0 left-0 w-full h-full object-cover ${isVideoLoaded && !isMenuOpen ? 'block' : 'hidden'}`}
+        className={`absolute top-0 left-0 w-full h-full object-cover ${isVideoLoaded && !isMenuOpen && !showImage ? 'block' : 'hidden'}`}
         src={backgroundVideo}
         autoPlay
         loop
