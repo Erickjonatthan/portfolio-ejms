7a662cdb66bcc529d4dba2b683ceba29da2a3bae
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-04-05 17:23:47 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,7 +77,7 @@ const Header = React.memo(() => {
                   <li key={section}>
                     <button
                       onClick={() => scrollToSection(section)}
-                      className={`relative text-2xl hover:text-neonBlue hover:after:scale-x-100 hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left ${
+                      className={`relative text-2xl sm:text-xl hover:text-neonBlue hover:after:scale-x-100 hover:after:shadow-neon after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-neonBlue after:bottom-0 after:left-0 after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:text-shadow-neon text-left ${
                         activeSection === section
                           ? "text-neonBlue after:scale-x-100 after:shadow-neon"
                           : ""
