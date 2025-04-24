08b47f67c4d8a3e5fb99c3c71cab0c75bd200708
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-04-24 19:29:08 -0300

    refactor(Header): adicionar nova opção "Cientista de Dados" ao menu de navegação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,6 +53,8 @@ export default function Header() {
                     1000,
                     "Freelancer",
                     1000,
+                    "Cientista de Dados",
+                    1000,
                     "Pesquisador Científico",
                     1000,
                   ]}
