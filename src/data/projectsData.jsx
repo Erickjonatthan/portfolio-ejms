fa73bb187f68652754dfb6f074ac74c3522450f1
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-04-24 15:25:02 -0300

    refactor(projectsData): update technologies icons for better accuracy
    Update the technologies icons in the projectsData.jsx file to more accurately reflect the technologies used in each project. This includes adding new icons and replacing outdated ones to ensure consistency and clarity in the project descriptions.

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,4 +1,8 @@
-import { FaReact, FaNodeJs, FaPython, FaDatabase, FaJava} from 'react-icons/fa';
+import { FaReact, FaNodeJs, FaPython, FaDatabase, FaJava, FaWhatsapp, FaHtml5, FaCss3, FaPhp  } from 'react-icons/fa';
+import { FaFlutter } from "react-icons/fa6";
+import { SiSpring, SiGodotengine, SiFlask  } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+
 
 export const projectsData = {
   backend: [
@@ -6,25 +10,25 @@ export const projectsData = {
       title: "API REST - AlfaEduca",
       description: "API Spring Boot com autenticação JWT e Docker. Integra PostgreSQL, Swagger e Tesseract OCR para criação de atividades educacionais.",
       image: "/assets/alfaeducaserver.png",
-      technologies: [<FaJava />, <FaDatabase />],
+      technologies: [<FaJava />, <FaDatabase />, <SiSpring />],
     },
     {
       title: "Lembrete de remédios",
       description: "API RESTful para gerenciamento de medicamentos com Spring Data e JPA. Arquitetura modular seguindo princípios SOLID e padrões de design.",
       image: "/assets/lembreteremedios.webp",
-      technologies: [<FaJava />, <FaDatabase />],
+      technologies: [<FaJava />, <FaDatabase />, <SiSpring />],
     },
     {
       title: "Gerenciamento de Viagens",
       description: "API REST para controle de viagens com Docker e PostgreSQL. Implementação CRUD seguindo princípios SOLID e padrões de design.",
       image: "/assets/travel_icon.svg",
-      technologies: [<FaJava />, <FaDatabase />],
+      technologies: [<FaJava />, <FaDatabase />, <SiSpring />],
     },
     {
       title: "Sistema de Certificação",
       description: "API Spring Boot escalável para gestão de certificados. Endpoints REST com PostgreSQL e padrões de arquitetura.",
       image: "/assets/certificado.jpg",
-      technologies: [<FaJava />, <FaDatabase />],
+      technologies: [<FaJava />, <FaDatabase />, <SiSpring />],
     },
     {
       title: "Challenge ONE | Hotel Alura",
@@ -38,25 +42,25 @@ export const projectsData = {
       title: "SaaS para Óticas",
       description: "Sistema de gestão para óticas com integração WhatsApp. Foco em renovação de exames e organização de vendas.",
       image: "/assets/saasoticas.jpg",
-      technologies: [<FaReact />, <FaNodeJs />],
+      technologies: [<FaFlutter />, <FaWhatsapp />],
     },
     {
       title: "Fort Pisos Laminados",
       description: "Página web com o objetivo de converter visitantes em leads ou clientes para uma empresa de Pisos Laminados.",
       image: "/assets/fortpisos.png",
-      technologies: [<FaReact />, <FaNodeJs />],
+      technologies: [<FaHtml5  />, <FaCss3 />, <IoLogoJavascript /> ],
     },
     {
       title: "Monitor&A",
       description: "Plataforma de interação acadêmica com fórum integrado. Centraliza discussões e compartilhamento de materiais entre estudantes e professores.",
       image: "/assets/monitor&a.jpg",
-      technologies: [<FaReact />],
+      technologies: [<FaHtml5  />, <FaCss3 />, <IoLogoJavascript />, <FaPhp /> ],
     },
     {
       title: "Decodificador de texto Alura",
       description: "Aplicação de criptografia textual para mensagens secretas. Desenvolvido como desafio do programa ONE.",
       image: "/assets/decodificador.png",
-      technologies: [<FaReact />],
+      technologies: [<FaHtml5  />, <FaCss3 />, <IoLogoJavascript /> ],
     },
   ],
   mobile: [
@@ -76,7 +80,7 @@ export const projectsData = {
       title: "Lembretes de Remédios",
       description: "Aplicativo para gerenciamento de lembretes de remédios.",
       image: "/assets/remedios.jpg",
-      technologies: [<FaReact />, <FaNodeJs />],
+      technologies: [<SiGodotengine />],
     },
   ],
   dataAnalysis: [
@@ -84,7 +88,7 @@ export const projectsData = {
       title: "Pale Blue Dot",
       description: "Visualização de dados da desertificação no NE brasileiro. Projeto premiado na competição de Visualização da NASA.",
       image: "/assets/nasa.png",
-      technologies: [<FaPython />, <FaDatabase />],
+      technologies: [<FaPython />, <FaDatabase />, <SiFlask />],
     },
   ],
 };
